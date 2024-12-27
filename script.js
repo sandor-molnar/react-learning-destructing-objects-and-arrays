0235a0890c0047186575367fdcbbd578e5ab81be
Molnár Sándor Benjámin <molnar.sandor.benjamin@gmail.com>
2024-12-27 12:41:00 +0100

    init

---
+++ script.js
@@ -0,0 +1,162 @@
+const data = [
+    {
+        id: 1,
+        title: "The Lord of the Rings",
+        publicationDate: "1954-07-29",
+        author: "J. R. R. Tolkien",
+        genres: [
+            "fantasy",
+            "high-fantasy",
+            "adventure",
+            "fiction",
+            "novels",
+            "literature",
+        ],
+        hasMovieAdaptation: true,
+        pages: 1216,
+        translations: {
+            spanish: "...",
+            chinese: "...",
+            french: "...",
+        },
+        reviews: {
+            goodreads: {
+                rating: 4.52,
+                ratingsCount: 630994,
+                reviewsCount: 13417,
+            },
+            librarything: {
+                rating: 4.53,
+                ratingsCount: 47166,
+                reviewsCount: 452,
+            },
+        },
+    },
+    {
+        id: 2,
+        title: "The Cyberiad",
+        publicationDate: "1965-01-01",
+        author: "Stanislaw Lem",
+        genres: [
+            "science fiction",
+            "humor",
+            "speculative fiction",
+            "fantasy",
+        ],
+        hasMovieAdaptation: false,
+        pages: 295,
+        translations: {},
+        reviews: {
+            goodreads: {
+                rating: 4.16,
+                ratingsCount: 11663,
+                reviewsCount: 812,
+            },
+            librarything: {
+                rating: 4.13,
+                ratingsCount: 2434,
+                reviewsCount: 0,
+            },
+        },
+    },
+    {
+        id: 3,
+        title: "Dune",
+        publicationDate: "1965-01-01",
+        author: "Frank Herbert",
+        genres: [
+            "science fiction",
+            "novel",
+            "adventure",
+        ],
+        hasMovieAdaptation: true,
+        pages: 658,
+        translations: {
+            spanish: "",
+        },
+        reviews: {
+            goodreads: {
+                rating: 4.25,
+                ratingsCount: 1142893,
+                reviewsCount: 49701,
+            },
+        },
+    },
+    {
+        id: 4,
+        title: "Harry Potter and the Philosopher's Stone",
+        publicationDate: "1997-06-26",
+        author: "J. K. Rowling",
+        genres: [
+            "fantasy",
+            "adventure",
+        ],
+        hasMovieAdaptation: true,
+        pages: 223,
+        translations: {
+            spanish: "...",
+            korean: "...",
+            bengali: "...",
+            portuguese: "...",
+        },
+        reviews: {
+            goodreads: {
+                rating: 4.47,
+                ratingsCount: 8910059,
+                reviewsCount: 140625,
+            },
+            librarything: {
+                rating: 4.29,
+                ratingsCount: 120941,
+                reviewsCount: 1960,
+            },
+        },
+    },
+    {
+        id: 5,
+        title: "A Game of Thrones",
+        publicationDate: "1996-08-01",
+        author: "George R. R. Martin",
+        genres: [
+            "fantasy",
+            "high-fantasy",
+            "novel",
+            "fantasy fiction",
+        ],
+        hasMovieAdaptation: true,
+        pages: 835,
+        translations: {
+            korean: "...",
+            polish: "...",
+            portuguese: "...",
+            spanish: "...",
+        },
+        reviews: {
+            goodreads: {
+                rating: 4.44,
+                ratingsCount: 2295233,
+                reviewsCount: 5958,
+            },
+            librarything: {
+                rating: 4.36,
+                ratingsCount: 38358,
+                reviewsCount: 1095,
+            },
+        },
+    },
+];
+
+function getBooks() {
+    return data;
+}
+
+function getBook(id) {
+    return data.find((book) => book.id === id);
+}
+
+const book = getBook(2);
+const { title, author, pages, publicationDate, genres, hasMovieAdaptation} = book;
+const [ primaryGenre, secondaryGenre ] = genres;
+
+console.log(title);
+console.log(primaryGenre, ', ', secondaryGenre);
